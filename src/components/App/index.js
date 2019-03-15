4c98a0b7efadd6878ce45a5101ef6cd516761a40
Jonathan Lester <jonathanmarklester@gmail.com>
2019-03-15 16:26:20 +0000

    passing lat/longs to citiesList component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-//import { Route, Redirect, Link } from "react-router-dom";
 import CitiesList from "../CitiesList";
 import "./App.css";
 
@@ -22,22 +21,19 @@ class App extends Component {
         };
     }
 
-    getLatLongOfCity = city => {
-        fetch(`${mqUrl}key=${mqKey}&location=${city}`).then(res =>
-            res
-                .json()
-                .then(data => data.results[0].locations)
-                .then(matches =>
-                    matches.filter(item => item.geocodeQuality === "CITY")
-                )
-                .then(info => info[0].latLng)
-                .then(latLong =>
-                    this.setState({
-                        latLongs: [...this.state.latLongs, latLong]
-                    })
-                )
-        );
-    };
+    getLatLongOfCity(city) {
+        fetch(`${mqUrl}key=${mqKey}&location=${city}`)
+            .then(res => res.json())
+            .then(data => data.results[0].locations)
+            .then(matches =>
+                matches.filter(item => item.geocodeQuality === "CITY")
+            )
+            .then(info =>
+                this.setState({
+                    latLongs: [...this.state.latLongs, info[0].latLng]
+                })
+            );
+    }
 
     selectCity() {
         this.setState({
@@ -45,15 +41,15 @@ class App extends Component {
         });
     }
 
-    componentDidMount() {
-        this.state.favourites.map(city => this.getLatLongOfCity(city));
+    async componentDidMount() {
+        await this.state.favourites.map(city => this.getLatLongOfCity(city));
     }
 
     render() {
         return (
             <div className="App">
                 Weather
-                <CitiesList cities={this.state.cities} />
+                <CitiesList cities={this.state.latLongs} />
             </div>
         );
     }
